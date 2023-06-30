0665e6ec403fb0db1160317462e4ca507047f4d3
Arif Ameer Hussain <aaazzz.arif0311@gmail.com>
2023-06-30 13:05:35 +0500

    Included display card function

--- BlackJack/blackjack.js
+++ BlackJack/blackjack.js
@@ -1,7 +1,25 @@
-let player = {
-    name : "",
-    chip : 0
-}
+// import cardImg from './cards'
+
+// let player = {
+//     name : "",
+//     chip : 0
+// }
+
+const cardImg = [
+    "1_spades.png",
+    "2_spades.png",
+    "3_spades.png",
+    "4_spades.png",
+    "5_spades.png",
+    "6_spades.png",
+    "7_spades.png",
+    "8_spades.png",
+    "9_spades.png",
+    "10_spades.png",
+    "11_spades.png",
+    "12_spades.png",
+    "13_spades.png",
+]
 
 let cards = []
 let isAlive = false;
@@ -15,9 +33,12 @@ let playerEl = document.getElementById("player")
 const startGame = () => {
     sum = 0
     cards = []
+    document.getElementById('dis-card').innerHTML = ""
     isAlive = true
     cards[0] = randomCard()
     cards[1] = randomCard()
+    displayCards(cards[0])
+    displayCards(cards[1])
     sum = cards[0] + cards[1]
     renderGame()
 }
@@ -45,6 +66,7 @@ const renderGame = () => {
 const newCard = () => {
     if(!isAlive ||isBlackJack)return;
     let card = randomCard()
+    displayCards(card)
     sum += card
     cards.push(card)
     renderGame()
@@ -60,3 +82,17 @@ const randomCard = () => {
     return card
 }
 
+const displayCards = (card) => {
+    card += '_spades.png'
+    const imageContainer = document.getElementById('dis-card')
+
+    for(let cI of cardImg){
+        if(card === cI)
+        {
+            const img = document.createElement('img')
+            img.src = 'images/'+card
+            imageContainer.appendChild(img);
+            break;
+        }
+    }
+}
---
+++ BlackJack/cards.js
@@ -0,0 +1,15 @@
+const cardImg = [
+    "1_spades.png",
+    "2_spades.png",
+    "3_spades.png",
+    "4_spades.png",
+    "5_spades.png",
+    "6_spades.png",
+    "7_spades.png",
+    "8_spades.png",
+    "9_spades.png",
+    "10_spades.png",
+    "11_spades.png",
+    "12_spades.png",
+    "13_spades.png",
+]
